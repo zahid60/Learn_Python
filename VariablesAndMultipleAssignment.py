5f4514ffc29d0801bd283e052c15d3a884ab66eb
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-09-29 22:27:28 +0600

    Add files via upload

---
+++ VariablesAndMultipleAssignment.py
@@ -0,0 +1,20 @@
+age = 20
+print(age)
+
+sentence = "My Name is Zahid"
+print(sentence)
+
+zahid = 34
+Hasan = 30
+Shanto = 22
+
+print(zahid)
+
+zahid, Hasan, Shanto = 34, 30, 22
+print(Hasan)
+
+zahid = hasan = shanto = 24
+print(zahid)
+
+name, age = "Zahid", 25
+print(age, name)

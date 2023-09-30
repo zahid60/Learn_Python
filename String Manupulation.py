c5c64347155e82378b795a8ea36e51a902b617e6
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-09-30 22:01:33 +0600

    Add files via upload

---
+++ String Manupulation.py
@@ -0,0 +1,16 @@
+print("I Love Pizza")
+
+sentence = "I love pizza"
+print(sentence)
+
+print(sentence.upper())
+
+sentence_2 = "I LOVE piZza And Nachos"
+print(sentence_2.lower())
+print(sentence_2.islower()) #Chack  all word is lowercase
+
+print(sentence_2.replace("piZza","Burger")) # Replace word
+
+print(sentence_2.index("p")) #Indexing
+
+print("I Love Pizza\n and nachos\n and Bike.")

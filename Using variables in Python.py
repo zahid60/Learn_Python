d081409ecbad110fceab6ec717d280fe2ecf5aab
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-09-30 20:26:08 +0600

    Add files via upload

---
+++ Using variables in Python.py
@@ -0,0 +1,10 @@
+person_name = "MD Zahid"
+object_1 = "car"
+object_1_color = "red"
+object_name_2 = "house"
+object_2_age = "25 years old"
+
+print("Hello, " + person_name)
+print("i am very interested by your " + object_1 + "is it " + object_1_color + ".")
+print("also " + person_name + " is your " + object_name_2 + " older than " + object_2_age)
+print("Thank you, " + person_name)

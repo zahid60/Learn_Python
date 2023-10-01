71e4c3b9df4108c0880f83269e5b9b1f719444ab
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-10-01 22:52:14 +0600

    Add files via upload

---
+++ Understanding the input function on python.py
@@ -0,0 +1,9 @@
+birthday = input("What is your Birthday: ")
+print("My age is " + birthday)
+
+name = input("What is  your name: ")
+age2 = input("What is your age: ")
+food = input("What do you want to eat: ")
+drink = input("What do you want to drink: ")
+
+print("Hello, My name is " + name + " I am " + age2 + " I want to eat " + food + " I want to drink " + drink)

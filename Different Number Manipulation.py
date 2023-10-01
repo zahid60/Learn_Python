ffbf36e1275336c0cd969adf48c3752777dc64aa
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-10-01 19:14:33 +0600

    Add files via upload

---
+++ Different Number Manipulation.py
@@ -0,0 +1,25 @@
+import math
+from math import *
+
+age = 20 + 10
+print("Hello,My age is  " + str(age))
+
+# Find Min,Max Number
+print(min(1, 0, -2, -2.444, 10))
+print(max(2, -1, -4, 5, 20))
+
+print(abs(-round(52 / 36 + pow(5, 2))))
+
+print(sqrt(25))
+
+print(factorial(3))
+
+print(math.isclose(1.233, 1.4566))
+print(math.isclose(1.233, 1.233))
+
+sequence = (2, 2, 2)
+print(math.prod(sequence))
+
+print(math.radians(180))
+print(math.radians(100.03))
+print(math.radians(-20))

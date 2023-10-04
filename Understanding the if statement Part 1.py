90b9631667c6710693795e075ce107d651186fe1
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-10-05 00:32:13 +0600

    Add files via upload

---
+++ Understanding the if statement Part 1.py
@@ -0,0 +1,9 @@
+I_want_to_eat = False
+I_want_to_drink = True
+
+if I_want_to_eat:
+    print("Lets have a pizza")
+elif I_want_to_drink:
+    print("Lets go to resturant")
+else:
+    print("I am Good")

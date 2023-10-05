bc8a57dab2b22af733fa77c62fc44b1e1db5d6c6
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-10-06 00:09:19 +0600

    Add files via upload

---
+++ Working with lists.py
@@ -0,0 +1,10 @@
+variable_1 = "apple"
+list1 = ["apple", "banana", "grapes", "lemom"]
+
+print(list1[1])
+
+list1[3] = "avacado"  # change list element
+list1[2] = "tomato"
+print(list1)
+
+print(list1[2:])  # list start second element

4d1992822140147181df688097ac7a6c577f5967
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-10-07 00:51:25 +0600

    Add files via upload

---
+++ What is dictionary in python.py
@@ -0,0 +1,16 @@
+color = {
+    "B": "blue",
+    "R": "Red",
+    "o": "orange"
+}
+
+print(color.get("B", "stop"))
+
+names = {
+    122: "Name_A",
+    123: "Name_B",
+    152: "Name_C",
+    144: "Name_D"
+}
+
+print(names.get(99, "This Number is not exist"))

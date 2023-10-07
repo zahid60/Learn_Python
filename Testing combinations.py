43098da024b27308e49146fab311ab2cfe39b064
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-10-08 01:24:27 +0600

    Add files via upload

---
+++ Testing combinations.py
@@ -0,0 +1,8 @@
+meal = ["pizza", "burger", "chicken"]
+drink = ["water", "juice", "cola"]
+dessert = ["cake", "ice", "chips"]
+
+for m in meal:
+    for d in drink:
+        for ds in dessert:
+            print("I will have " + m + " as a meal " + " I also want " + d + " as a drink and " + ds + " as a dessert")

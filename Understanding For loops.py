6e1abb5188acf4e60d6b7ac3ede205501e0680bc
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-10-08 01:23:40 +0600

    Add files via upload

---
+++ Understanding For loops.py
@@ -0,0 +1,19 @@
+# 1
+color = ["blue", "red"]
+
+for x in color:
+    print(x)
+# 2
+for letter in "Blue":
+    print(letter)
+# 3
+colors = ["blue", "red", "yellow", "green"]
+
+for one_color in colors:
+    print(one_color)
+    if one_color == "red":
+        break
+
+# 4
+for number in range(6, 20):
+    print(number)

203064df6e9e4d0eab5ff80924b50b744f19c8dc
Zahid Hasan <71926628+zahid60@users.noreply.github.com>
2023-10-09 21:06:38 +0600

    Add files via upload

---
+++ Working with classes and objects.py
@@ -0,0 +1,8 @@
+# Object fetch data from class
+from pizzaClass import pizza1
+
+pizzaV = pizza1("vegeteriam", 5, "small")
+pizzaP = pizza1("matton", 12, "large")
+
+print(pizzaV.type)
+print(pizzaP.size)
---
+++ pizzaClass.py
@@ -0,0 +1,7 @@
+# Class is Set of attributes[Class is simply for the product]
+
+class pizza1:
+    def __init__(self, type, price, size):
+        self.type = type
+        self.price = price
+        self.size = size
